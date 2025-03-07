b2c0a7dc0ba241d9d054dec182226ecc35382c35
AXLMRIN <axelmorinmail@gmail.com>
2025-03-07 17:38:27 +0100

    started a script

---
+++ src/316_ideology.py
@@ -0,0 +1,12 @@
+# TODO Réaliser un bandeau
+
+# IMPORTS --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
+# Third parties
+from datasets import load_dataset
+from pandas import read_csv
+# Native
+
+# Custom
+
+# SCRIPT --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
+dataset = read_csv("data/316_ideological_book_corpus/ibc.csv")
