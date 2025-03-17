9aa7fb87da2eac0a445ee305558955f51613c729
AXLMRIN <axelmorinmail@gmail.com>
2025-03-17 17:12:43 +0100

    made available to change the training args

--- src/toolbox/AutoClassifierRoutineConfig.py
+++ src/toolbox/AutoClassifierRoutineConfig.py
@@ -3,6 +3,7 @@
 # Third parties
 from torch.cuda import is_available as gpu_available
 from transformers import TrainingArguments
+from mergedeep import merge
 
 # SCRIPT --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 class AutoClassifierRoutineConfig: 
@@ -12,7 +13,8 @@ class AutoClassifierRoutineConfig:
         label_col : str, 
         batch_size : int = 32, 
         only_train_classifier : bool = False, 
-        dev_mode : bool = False ) -> None:
+        dev_mode : bool = False,
+        **kwargs) -> None:
         """
         Config for AutoClassifierRoutine
         """
@@ -35,19 +37,23 @@ class AutoClassifierRoutineConfig:
         self.model_name = "answerdotai/ModernBERT-base"
         self.device : str = "cuda" if gpu_available() else "cpu"
         self.att_implementation : str = "sdpa" # FIXME Not implemented for now TODO look deeper
-        self.training_args = TrainingArguments(
+        
+        training_args : dict = {
             # Parameters that are customisable
-            output_dir = files["output_dir"],
-            save_strategy = "no" if self.dev_mode else "epoch",
-            num_train_epochs = 1 if self.dev_mode else 10,
+            "output_dir" : files["output_dir"],
+            "save_strategy" : "no" if self.dev_mode else "epoch",
+            "num_train_epochs" : 1 if self.dev_mode else 10,
             # Fixed parameters
-            overwrite_output_dir=True,
-            eval_strategy = "epoch",
-            learning_rate=2e-5,
-            per_device_train_batch_size=self.batch_size,
-            per_device_eval_batch_size=self.batch_size,
-            weight_decay=0.01,
-        ) # TODO continue digging the parameters
+            "overwrite_output_dir" : True,
+            "eval_strategy" : "epoch",
+            "learning_rate" : 2e-5,
+            "per_device_train_batch_size" : self.batch_size,
+            "per_device_eval_batch_size" : self.batch_size,
+            "weight_decay" : 0.01,
+        }
+        if "training_args" in kwargs:
+            training_args = merge(training_args, kwargs["training_args"])
+        self.training_args = TrainingArguments(**training_args) # TODO continue digging the parameters
         # Tokenizer related
         self.tokenizer_settings : dict = {
             "padding" : "max_length",
