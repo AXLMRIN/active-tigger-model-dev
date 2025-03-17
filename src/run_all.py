d789469d0a22220ae319b6d2c12a0432a068ede1
AXLMRIN <axelmorinmail@gmail.com>
2025-03-17 18:12:23 +0100

    added the four dataset

--- src/run_all.py
+++ src/run_all.py
@@ -4,7 +4,6 @@ from toolbox import (
 from mergedeep import merge
 
 general_args = {
-
     "batch_size" : 16,
     "num_train_epochs" : 10,
     "only_train_classifier" : False,
@@ -18,12 +17,43 @@ file_args = {
         "files" : {
             "open_local" : "data/316_ideological_book_corpus/ibc.csv",
             "open_s3" : "s3://projet-datalab-axel-morin/model_benchmarking/316_ideology/data.csv", 
-            "output_dir" : "2025-03-17-autoClassifier-test"
+            "output_dir" : "2025-03-17-autoClassifier-test-316"
+        }
+    },
+    "319" : {
+        "sentence_col" : "Tweet",
+        "label_col" : "Stance",
+        "files" : {
+            "open_local" : "data/319_semeval_stance/semeval_stance.csv",
+            "open_s3" : "s3/projet-datalab-axel-morin/model_benchmarking/319_stance/data.csv", 
+            "output_dir" : "2025-03-17-autoClassifier-test-319"
+        }
+    },
+    "333" : {
+        "sentence_col" : "content",
+        "label_col" : "bias_text",
+        "files" : {
+            "open_local" : "data/333_media_ideology/media_ideology.csv",
+            "open_s3" : "s3/projet-datalab-axel-morin/model_benchmarking/333_ideology/data.csv", 
+            "output_dir" : "2025-03-17-autoClassifier-test-333"
+        }
+    },
+    "334" : {
+        "sentence_col" : "Quotes",
+        "label_col" : "TropesHuman",
+        "files" : {
+            "open_local" : "data/334_tropes/tropes.csv",
+            "open_s3" : "s3/projet-datalab-axel-morin/model_benchmarking/334_roles/data.csv", 
+            "output_dir" : "2025-03-17-autoClassifier-test-334"
         }
     }
 }
 
 for test_id in file_args : 
+    print(("\n"
+        f"Running test number {test_id} =================\n\n"
+    ))
+
     args = merge(general_args, file_args[test_id])
     config = AutoClassifierRoutineConfig(**args)
     routine = AutoClassifierRoutine(config)
