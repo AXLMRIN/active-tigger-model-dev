15f42bff4d61e25cdae82f1480e82aab41e06d1d
AXLMRIN <axelmorinmail@gmail.com>
2025-03-18 11:19:30 +0100

    tokenizer length

--- src/run_all.py
+++ src/run_all.py
@@ -2,6 +2,7 @@ from toolbox import (
     AutoClassifierRoutineConfig, AutoClassifierRoutine
 )
 from mergedeep import merge
+from copy import deepcopy
 
 general_args = {
     "batch_size" : 16,
@@ -18,7 +19,8 @@ file_args = {
             "open_local" : "data/316_ideological_book_corpus/ibc.csv",
             "open_s3" : "s3://projet-datalab-axel-morin/model_benchmarking/316_ideology/data.csv", 
             "output_dir" : "2025-03-17-autoClassifier-test-316"
-        }
+        },
+        "tokenizer_max_length" : 100
     },
     "319" : {
         "sentence_col" : "Tweet",
@@ -27,7 +29,8 @@ file_args = {
             "open_local" : "data/319_semeval_stance/semeval_stance.csv",
             "open_s3" : "s3://projet-datalab-axel-morin/model_benchmarking/319_stance/data.csv", 
             "output_dir" : "2025-03-17-autoClassifier-test-319"
-        }
+        },
+        "tokenizer_max_length" : 60
     },
     "333" : {
         "sentence_col" : "content",
@@ -36,9 +39,10 @@ file_args = {
             "open_local" : "data/333_media_ideology/media_ideology.csv",
             "open_s3" : "s3://projet-datalab-axel-morin/model_benchmarking/333_ideology/data.csv", 
             "output_dir" : "2025-03-17-autoClassifier-test-333"
-        }
+        },
+        "tokenizer_max_length" : 2745
     },
-    # FIXME the preprocess is stuck
+    # FIXME might not be used 
     # "334" : {
     #     "sentence_col" : "Quotes",
     #     "label_col" : "TropesHuman",
@@ -54,8 +58,7 @@ for test_id in file_args :
     print(("\n"
         f"Running test number {test_id} =================\n\n"
     ))
-
-    args = merge(general_args, file_args[test_id])
+    args = merge(deepcopy(general_args), file_args[test_id])
     config = AutoClassifierRoutineConfig(**args)
     routine = AutoClassifierRoutine(config)
-    routine.run()
+    #routine.run()
