f2afebff376c6c0ed5fba91a3c16b7c9f5bbdf7d
AXLMRIN <axelmorinmail@gmail.com>
2025-03-24 21:49:10 +0100

    parameters for the dataset

--- src2/toolbox/Config.py
+++ src2/toolbox/Config.py
@@ -1,6 +1,16 @@
 class Config:
     def __init__(self):
-        self.__name = ""
+        self.dataset_filename : str = "data/316_ideological_book_corpus/ibc.csv" # UPGRADE make it editable 
+        self.dataset_label_col : str = "leaning"
+        self.dataset_text_col : str = "sentence"
+        self.seed : int = 2306406
+        self.split_parameters : dict = {
+            "proportion_train" : 0.7,
+            "proportion_test" : 0.15, 
+            "proportion_valid" : 0.15,
+            "shuffle" : True, 
+            "seed" : self.seed
+        }
     def __str__(self):
         return (
             "Config object"
