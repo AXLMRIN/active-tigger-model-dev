2a28fefad2b974cdf495a1cf5ec3437ed979bfe0
AXLMRIN <axelmorinmail@gmail.com>
2025-03-24 21:49:33 +0100

    new strategy initial commit

---
+++ src2/toolbox/CustomLogger.py
@@ -0,0 +1,7 @@
+class CustomLogger:
+    def __init__(self):
+        self.name = ""
+    def __str__(self) -> str:
+        return (
+            "Custom Logger object"
+        )
---
+++ src2/toolbox/general.py
@@ -0,0 +1,46 @@
+# IMPORTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Third parties
+from datasets import Dataset, DatasetDict
+
+# Native
+
+# Custom
+from .Config import Config
+
+# TYPES - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+def split_test_train_valid(dataset : Dataset, proportion_train : float = 0.7,
+    proportion_test : float = None, proportion_valid : float = None,
+    shuffle : bool = True, seed : int = 42) -> DatasetDict:
+    if (proportion_test is None) & (proportion_valid is None):
+        proportion_test = (1 - proportion_train) / 2
+        proportion_valid = proportion_test
+    elif (proportion_test is None) : 
+        proportion_test = 1 - proportion_train - proportion_valid
+    elif (proportion_valid is None) :
+        proportion_valid = 1 - proportion_train - proportion_test
+    try:
+        assert(proportion_train + proportion_test + proportion_valid == 1)
+    except:
+        print((
+            "WARNING Wrong entry. Your entries :\n"
+            "\t - proportion_train : {proportion_train}\n"
+            "\t - proportion_test : {proportion_test}\n"
+            "\t - proportion_valid : {proportion_valid}\n"
+            "\n"
+            "By default we use the tuple (0.7,0.15,0.15)"
+        ))
+        proportion_train, proportion_test, proportion_valid = 0.7,0.15,0.15
+
+    ds_temp = dataset.train_test_split(test_size = proportion_test, 
+                shuffle=shuffle, seed = seed)
+    ds_temp2 = ds_temp["train"].train_test_split(
+                        train_size = proportion_train / (1- proportion_test),
+                        shuffle = shuffle, seed = seed
+    )
+    ds = DatasetDict({
+        "train" : ds_temp2["train"],
+        "validation" : ds_temp2["test"],
+        "test" : ds_temp["test"],
+    })
+
+    return ds
