819ee4b117f6d9a2dc36ff55c923d3106ca507fe
AXLMRIN <axelmorinmail@gmail.com>
2025-03-25 12:18:01 +0100

    routine for development

---
+++ src2/routine_dev.py
@@ -0,0 +1,33 @@
+from toolbox.Config import Config
+from toolbox.CustomModel import CustomModel
+from toolbox.History import History
+from toolbox.CustomDataset import CustomDataset
+from toolbox.CustomClassifier import CustomClassifier
+from toolbox.CustomEmbedder import CustomEmbedder
+from copy import deepcopy
+config = Config()
+dataset = CustomDataset(config)
+dataset.open_dataset()
+dataset.find_labels()
+
+def preprocess_function_label(batch, label2id): 
+    return [label2id[label] for label in batch["label"]]
+def preprocess_function_text(batch): 
+    return [sentence.lower() for sentence in batch["text"]]
+
+LABEL2ID = deepcopy(dataset.label2id)
+dataset.preprocess_data(
+    preprocess_function_text, 
+    lambda batch : preprocess_function_label(batch, LABEL2ID)
+)
+del LABEL2ID
+print(dataset)
+
+classifier = CustomClassifier(config)
+print(classifier)
+embedder = CustomEmbedder(config)
+print(embedder)
+
+model = CustomModel(config, embedder, classifier)
+
+model.train(dataset.ds["test"])
