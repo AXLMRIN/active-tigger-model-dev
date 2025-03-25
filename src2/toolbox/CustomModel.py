00aca8d65036d0e96b7a7ff4be8a3e0c93da44ce
AXLMRIN <axelmorinmail@gmail.com>
2025-03-25 10:54:26 +0100

    implemented a prediction function

--- src2/toolbox/CustomModel.py
+++ src2/toolbox/CustomModel.py
@@ -1,8 +1,38 @@
+# IMPORTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Third parties
+from torch import Tensor
+from torch.cuda import synchronize, ipc_collect, empty_cache
+# Native
+import gc
+# Custom
+from .Config import Config
+from .CustomEmbedder import CustomEmbedder
+from .CustomClassifier import CustomClassifier
+from .History import History
+# CLASS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 class CustomModel:
-    def __init__(self) -> None:
-        self.__name = ""
+    def __init__(self, config : Config, embedder : CustomEmbedder, 
+                 classifier : CustomClassifier) -> None:
+        self.config = config
+        self.embedder = embedder
+        self.classifier = classifier
+        self.history = History()
+    
+    def predict(self, entries : list[str]):
+        embeddings : Tensor = self.embedder(entries) # shape(batch x config.embeddingmodel_dim)
+        logits = self.classifier(embeddings)
+        return logits
 
     def __str__(self) -> str:
         return (
             "Custom Model object"
         )
+
+    def clean(self):
+        self.embedder.clean()
+        del self.embedder, self.classifier
+        gc.collect()
+        if self.config.device == "cuda":
+            synchronize()
+            empty_cache()
+            ipc_collect()
