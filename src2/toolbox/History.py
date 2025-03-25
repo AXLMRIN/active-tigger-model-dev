85a008f9ecdeb513a456ac1322447018568f011e
AXLMRIN <axelmorinmail@gmail.com>
2025-03-25 12:14:25 +0100

    implemented train loss saving

--- src2/toolbox/History.py
+++ src2/toolbox/History.py
@@ -1,6 +1,14 @@
 class History:
     def __init__(self):
-        self.__name = ""
+        self.train_loss_per_epoch : dict[int:list[float]] = {}
+        self.train_loss_global : list[float] = []
+
+    def append_loss_train(self, epoch : int, loss_value) -> None:
+        self.train_loss_global.append(loss_value)
+        
+        if epoch not in self.train_loss_per_epoch:
+            self.train_loss_per_epoch[epoch] = []
+        self.train_loss_per_epoch[epoch].append(loss_value)
 
     def __str__(self) -> str:
         return (
