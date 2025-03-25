e5bcbf7ddd7bd3ede041e044bf71ae4751397959
AXLMRIN <axelmorinmail@gmail.com>
2025-03-25 09:47:23 +0100

    import all

--- src2/toolbox/__init__.py
+++ src2/toolbox/__init__.py
@@ -1,4 +1,6 @@
 from .Config import *
 from .CustomModel import *
 from .History import *
-from .CustomDataset import CustomDataset
+from .CustomDataset import *
+from .CustomClassifier import *
+from .CustomEmbedder import *
