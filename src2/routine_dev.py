9c31728304a9b9ef600b0f21731bdd2f0d58dab3
AXLMRIN <axelmorinmail@gmail.com>
2025-04-09 09:53:17 +0200

    update

--- src2/routine_dev.py
+++ src2/routine_dev.py
@@ -1,11 +1,18 @@
-# CUSTOM IMPORTS
 from toolbox.Config import Config
 from toolbox.CustomModel import CustomModel
+from toolbox.History import History
 from toolbox.CustomDataset import CustomDataset
 from toolbox.CustomClassifier import CustomClassifier
 from toolbox.CustomEmbedder import CustomEmbedder
 from copy import deepcopy
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from random import choice
+
 config = Config()
+config.model_train_n_epoch = 5 # After first result analysis
+
 dataset = CustomDataset(config)
 dataset.open_dataset()
 dataset.find_labels()
@@ -21,20 +28,20 @@ dataset.preprocess_data(
     lambda batch : preprocess_function_label(batch, LABEL2ID)
 )
 del LABEL2ID
-print(dataset)
+
 
 embedder = CustomEmbedder(config)
-print(embedder)
-
 classifier = CustomClassifier(config)
-print(classifier)
-
 model = CustomModel(config, embedder, classifier)
+model.train(dataset.ds["train"],dataset.ds["validation"])
 
-model.train(
-    dataset.ds["train"],
-    dataset.ds["validation"]
+test_loader = DataLoader(
+    dataset.ds["test"], 
+    batch_size=config.model_train_batchsize,
+    shuffle = True
 )
-model.history.plot_all()
-model.history.save_all("./testing-save")
+model.test_loop(test_loader)
+
+model.history.save_all(config.history_foldersave)
+config.save()
 model.clean()
