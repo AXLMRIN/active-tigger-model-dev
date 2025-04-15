6c2f02b54f59472496e720410687cdcf5791946f
AXLMRIN <axelmorinmail@gmail.com>
2025-04-16 00:35:53 +0200

    extended the routine

--- src2/sklearn_routine.py
+++ src2/sklearn_routine.py
@@ -4,56 +4,11 @@ from toolbox.CustomDataset import CustomDataset
 from toolbox.CustomClassifier import CustomClassifier
 from toolbox.CustomEmbedder import CustomEmbedder
 
-import pandas as pd
+import os
 from torch import no_grad, Tensor, concat, save
 from torch.utils.data import DataLoader
 
-config = Config()
-config.model_train_n_epoch = 5 # After first result analysis
-
-config.history_foldersave = "./sklearn_save"
-
-config = Config()
-config.model_train_n_epoch = 5 # After first result analysis
-
-config.history_foldersave = "./debug"
-config.embeddingmodel_save_filename = f"{config.history_foldersave}/{config.embeddingmodel_save_filename}"
-config.classifier_save_filename = f"{config.history_foldersave}/{config.classifier_save_filename}"
-
-config.model_train_embedding_adam_parameters["lr"] = 5e-5
-config.model_train_classifier_sgd_parameters["lr"] = 0.0003
-config.classifier_hiddenlayer_dim = 10
-
-dataset = CustomDataset(config)
-dataset.open_dataset()
-dataset.find_labels()
-
-def preprocess_function_label(batch, label2id): 
-    return [label2id[label] for label in batch["label"]]
-def preprocess_function_text(batch): 
-    return [sentence.lower() for sentence in batch["text"]]
-
-dataset.ds["train"] = dataset.ds["train"].select(range(40))
-dataset.ds["test"] = dataset.ds["test"].select(range(40))
-dataset.ds["validation"] = dataset.ds["validation"].select(range(40))
-
-dataset.preprocess_data(
-    preprocess_function_text, 
-    lambda batch : preprocess_function_label(batch, config.dataset_label2id)
-)
-
-for model_name in [
-    "google-bert/bert-base-uncased",
-    "answerdotai/ModernBERT-base",
-    "answerdotai/ModernBERT-large",
-    "nlptown/bert-base-multilingual-uncased-sentiment"
-                  ]:
-    config.embeddingmodel_name = model_name
-    embedder = CustomEmbedder(config)
-    classifier = CustomClassifier(config)
-    model = CustomModel(config, embedder, classifier)
-
-    def callback_function_save_tensors(epoch : int, dataloader : DataLoader, 
+def callback_function_save_tensors(epoch : int, dataloader : DataLoader, 
             model : CustomModel, filename : str) -> None: 
         output_list : Tensor|None = None
         idx = 0 
@@ -77,6 +32,60 @@ for model_name in [
             
         save(outputs_cat, f"{filename}_{epoch}")
 
+
+config = Config()
+config.model_train_n_epoch = 5 # After first result analysis
+
+config.history_foldersave = "./debug"
+config.embeddingmodel_save_filename = f"{config.history_foldersave}/{config.embeddingmodel_save_filename}"
+config.classifier_save_filename = f"{config.history_foldersave}/{config.classifier_save_filename}"
+
+config.model_train_embedding_adam_parameters["lr"] = 5e-5
+config.model_train_classifier_sgd_parameters["lr"] = 0.0003
+config.classifier_hiddenlayer_dim = 10
+
+dataset = CustomDataset(config)
+dataset.open_dataset()
+dataset.find_labels()
+
+def preprocess_function_label(batch, label2id): 
+    return [label2id[label] for label in batch["label"]]
+def preprocess_function_text(batch): 
+    return [sentence.lower() for sentence in batch["text"]]
+
+# debug ---
+dataset.ds["train"] = dataset.ds["train"].select(range(40))
+dataset.ds["test"] = dataset.ds["test"].select(range(40))
+dataset.ds["validation"] = dataset.ds["validation"].select(range(40))
+
+dataset.preprocess_data(
+    preprocess_function_text, 
+    lambda batch : preprocess_function_label(batch, config.dataset_label2id)
+)
+
+if "sklearn_save" not in os.listdir("./") : 
+     os.mkdir("./sklearn_save")
+
+for model_name in [
+    "google-bert/bert-base-uncased",
+    "answerdotai/ModernBERT-base",
+    "answerdotai/ModernBERT-large",
+    "nlptown/bert-base-multilingual-uncased-sentiment",
+    "FacebookAI/roberta-base",
+    "FacebookAI/roberta-large",
+    "FacebookAI/xlm-roberta-large"
+                  ]:
+    
+    if model_name not in os.listdir("./sklearn_save") : 
+         os.mkdir(f"./sklearn_save/{model_name}")
+
+    config.embeddingmodel_name = model_name
+    embedder = CustomEmbedder(config)
+    classifier = CustomClassifier(config)
+    model = CustomModel(config, embedder, classifier)
+
+    
+
     callback_function_save_tensors(-1,
         DataLoader(
             dataset=dataset.ds["train"], 
@@ -84,7 +93,7 @@ for model_name in [
             batch_size = config.model_train_batchsize
         ),
         embedder,
-        "./sklearn_save_epoch"
+        f"./sklearn_save/{model_name}/epoch"
     )
     model.train(dataset.ds["train"],dataset.ds["validation"],
         callback_function=callback_function_save_tensors,
@@ -92,5 +101,5 @@ for model_name in [
             "dataloader" : DataLoader(dataset.ds["train"], shuffle = True, 
                                     batch_size = config.model_train_batchsize),
             "model" : embedder,
-            "filename" : "./sklearn_save_epoch"
+            "filename" : f"./sklearn_save/{model_name}/epoch"
         })
