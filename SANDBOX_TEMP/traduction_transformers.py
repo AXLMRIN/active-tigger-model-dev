01c8e8555c049d65caffa57635ff5f4f80c85afe
AXLMRIN <axelmorinmail@gmail.com>
2025-04-30 11:18:31 +0200

    GA run

--- SANDBOX_TEMP/traduction_transformers.py
+++ SANDBOX_TEMP/traduction_transformers.py
@@ -1,16 +1,22 @@
-from transformer_class import dataset, transformer
+from transformer_class import dataset, transformer, CustomLogger
 from datasets.utils.logging import disable_progress_bar
 from torch import Tensor
 from transformer_functions import multi_label_metrics
+import pygad 
 
 disable_progress_bar()
 
-def fitness(ga_instance, solution, solution_idx):
+# Solution
+# 0 - learning rate
+# 1 - weight decay
+
+def fitness_func(ga_instance, solution, solution_idx):
     ds = dataset("dataUNSAFE/ibc.csv",
                 col_text = "sentence", 
                 col_label = "leaning")
 
     tr = transformer(ds, "google-bert/bert-base-uncased")
+
     def preprocess(batch_of_rows : dict):
         """For now we only uncapitalised the sentences"""
         batch_of_rows["text"] = [sentence.lower() 
@@ -19,7 +25,10 @@ def fitness(ga_instance, solution, solution_idx):
 
     tr.preprocess(preprocess)
     tr.encode()
-    tr.debug_mode()
+
+    tr.training_argslearning_rate = solution[0]
+    tr.training_args.weight_decay = solution[1]
+    
     tr.train()
 
     output = tr.model(**{
@@ -33,3 +42,40 @@ def fitness(ga_instance, solution, solution_idx):
         output.logits.to(device="cpu"),
         tr.encoded_dataset["eval"]["labels"]
         )['f1']
+
+GA_parameters = {
+    #Must Specify
+    'fitness_func' : fitness_func,
+    'num_generations' : 50,
+    
+    'sol_per_pop' : 10,
+    'num_parents_mating' : 2,
+    'keep_elitism' : 1,
+    
+    'num_genes' : 2,
+    "gene_space" : [
+        {'low' : 1e-6, 'high' : 1e-4},
+        [0.1,0.2,0.3,0.4]
+    ],
+    "stop_criteria" : "saturate_5",
+    # Default
+    'mutation_type' : "random",
+    'parent_selection_type' : "sss",
+    'crossover_type' : "single_point",
+    'Ò' : 50,
+    # Other
+    'save_solutions' : False,
+    'random_seed' : 2306406
+}
+
+ga_instance = pygad.GA(**GA_parameters)
+
+try : 
+    ga_instance.run()
+    ga_instance.save("2025-04-28-fine_tuning")
+    
+    CustomLogger().notify_when_done(f"Run finished\n{ga_instance.best_solution()}")
+
+except:
+    CustomLogger().notify_when_done("Run failed")
+
--- SANDBOX_TEMP/transformer_class.py
+++ SANDBOX_TEMP/transformer_class.py
@@ -162,3 +162,34 @@ class transformer:
             # optimizers = 
         )
         return trainer.train()
+
+
+class CustomLogger:
+    def __init__(self):
+        self.name = ""
+
+    def notify_when_done(self, message : str = '') : 
+        """send an email when finished"""
+        subj = "Onyxia run — done"
+        body = ("https://projet-datalab-axel-morin-135428-0.lab.groupe-genes.fr/lab?"
+                f"\n{message}")
+        em = EmailMessage()
+        em["From"] = os.environ["EMAIL_FROM"]
+        em["To"] = os.environ["EMAIL_TO"]
+        em["Subject"] = subj
+        em.set_content(body)
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp : 
+            print(smtp.login(os.environ["EMAIL_FROM"], os.environ["EMAIL_FROM_PWD"]))
+            print(smtp.sendmail(
+                os.environ["EMAIL_FROM"],
+                os.environ["EMAIL_TO"], 
+                em.as_string())
+            )
+
+    def __str__(self) -> str:
+        return (
+            "Custom Logger object"
+        )
