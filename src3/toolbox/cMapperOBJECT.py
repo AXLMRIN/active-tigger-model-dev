e893bad536d6cfee5e5d67b682b59e05ea334c1a
AXLMRIN <axelmorinmail@gmail.com>
2025-05-18 12:27:17 +0000

    bug fix

--- src3/toolbox/cMapperOBJECT.py
+++ src3/toolbox/cMapperOBJECT.py
@@ -4,5 +4,5 @@ class cMapper:
         self.functions = functions
 
     def __call__(self, idx, value):
-        try: return {self.keys[idx] : function[idx](value)}
+        try: return {self.keys[idx] : self.functions[idx](value)}
         except: raise(KeyError, "custom mapping idx not right")
