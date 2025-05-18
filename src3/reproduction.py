4ab7b3998f8ec91fa77e1dc0107d3e666a8daa6b
AXLMRIN <axelmorinmail@gmail.com>
2025-05-18 17:27:46 +0200

    changed the n_sample range

--- src3/reproduction.py
+++ src3/reproduction.py
@@ -56,7 +56,7 @@ for model in all_models:
                 routineRandomForest = routine(
                     folder_name = f"{model}-{lr}-data",
                     classifier = RandomForestClassifier, 
-                    n_sample_range = [250,500,750,1000,1500],
+                    n_sample_range = [500,1000,1500],
                     epoch_range = [0,1,2,3,4,5],
                     GA_parameters = GA_p,
                     custom_mapping = mapper,
@@ -118,7 +118,7 @@ for model in all_models:
                 routineKNN = routine(
                     folder_name = f"{model}-{lr}-data",
                     classifier = KNeighborsClassifier, 
-                    n_sample_range = [250,500,750,1000,1500],
+                    n_sample_range = [500,1000,1500],
                     epoch_range = [0,1,2,3,4,5],
                     GA_parameters = GA_p,
                     custom_mapping = mapper,
@@ -176,7 +176,7 @@ for model in all_models:
                 routineOneLayer = routineNotOptmisied(
                      folder_name = f"{model}-{lr}-data",
                      classifier = MLPClassifier,
-                     n_sample_range = [250,500,750,1000,1500],
+                     n_sample_range = [500,1000,1500],
                      epoch_range = [0,1,2,3,4,5],
                      classifier_parameters = {
                           "hidden_layer_sizes" : (),
