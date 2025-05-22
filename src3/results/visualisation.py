aa577b37e564b5f921acfab367176bc5a157d241
AXLMRIN <axelmorinmail@gmail.com>
2025-05-22 09:53:24 +0200

    dynamic n_best

--- src3/results/visualisation.py
+++ src3/results/visualisation.py
@@ -15,33 +15,36 @@ data = vt.genData({
     "MLPClassifier (HF)" : "2025-05-20-HuggingFaceClassification.csv"
 })
 
+N_best = 20
+
 all_figures = {}
+
 all_figures["comparaison_plongement_classification_all"] = \
     vt.f1_macro_per_model_and_method(data, N = None)
-all_figures["comparaison_plongement_classification_200"] = \
-    vt.f1_macro_per_model_and_method(data, N = 200)
+all_figures["comparaison_plongement_classification_N_best"] = \
+    vt.f1_macro_per_model_and_method(data, N = N_best)
 
 all_figures["comparaison_plongement_n_sample_all"] = \
     vt.f1_macro_per_n_sample_and_method(data, N = None)
-all_figures["comparaison_plongement_n_sample_200"] = \
-    vt.f1_macro_per_n_sample_and_method(data, N = 200)
+all_figures["comparaison_plongement_n_sample_N_best"] = \
+    vt.f1_macro_per_n_sample_and_method(data, N = N_best)
 
 all_figures["comparaison_lr_model_method_all"] = \
     vt.f1_macro_lr_per_model_and_method(data, N = None)
-all_figures["comparaison_lr_model_method_200"] = \
-    vt.f1_macro_lr_per_model_and_method(data, N = 200)
+all_figures["comparaison_lr_model_method_N_best"] = \
+    vt.f1_macro_lr_per_model_and_method(data, N = N_best)
 all_figures["comparaison_lr_model_all"] = \
     vt.f1_macro_lr_per_model(data, N = None)
-all_figures["comparaison_lr_model_200"] = \
-    vt.f1_macro_lr_per_model(data, N = 200)
+all_figures["comparaison_lr_model_N_best"] = \
+    vt.f1_macro_lr_per_model(data, N = N_best)
 
 all_figures["comparaison_epoch_model_method_all"] = \
     vt.f1_macro_epoch_per_model_and_method(data, N = None)
-all_figures["comparaison_epoch_model_method_200"] = \
-    vt.f1_macro_epoch_per_model_and_method(data, N = 200)
+all_figures["comparaison_epoch_model_method_N_best"] = \
+    vt.f1_macro_epoch_per_model_and_method(data, N = N_best)
 all_figures["comparaison_epoch_model_all"] = \
     vt.f1_macro_epoch_per_model(data, N = None)
-all_figures["comparaison_epoch_model_200"] = \
-    vt.f1_macro_epoch_per_model(data, N = 200)
+all_figures["comparaison_epoch_model_N_best"] = \
+    vt.f1_macro_epoch_per_model(data, N = N_best)
 
-vt.export(all_figures)
+vt.export(all_figures, N_best)
