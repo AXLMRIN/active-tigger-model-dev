f059d3abffd77965b450feec9c6f6555d063dfed
AXLMRIN <axelmorinmail@gmail.com>
2025-05-24 20:45:33 +0200

    removed debug

--- src3/results/visualiseThis/data_handling.py
+++ src3/results/visualiseThis/data_handling.py
@@ -18,8 +18,7 @@ def fetch_data(filename_dictionnary : dict[str:str], usecols : list[str]) :
         new_df["model"] = new_df["filename"].apply(lambda x : "-".join(x.split("-")[3:-3]))
         new_df["lr"] = new_df["filename"].apply(lambda x : "-".join(x.split("-")[-3:-1]))
         if df is None: df = new_df
-
-        print(len(new_df), len(df))
+        else: df = pd.concat((df, new_df))
 
     return df
     
