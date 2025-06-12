625b8b7a1bcf1f8b80538b2ad37d7d9324904c1f
AXLMRIN <axelmorinmail@gmail.com>
2025-06-12 17:00:00 +0200

    initial commit

---
+++ src/toolbox/general.py
@@ -0,0 +1,16 @@
+# IMPORTS ######################################################################
+import pandas as pd
+from typing import Any
+from json import dumps
+from numpy.random import shuffle
+
+# SCRIPTS ######################################################################
+def IdentityFunction(x : Any) -> Any: 
+    return x
+
+def pretty_printing_dictionnary(d : dict) -> str:
+    return dumps(d, sort_keys = False, indent = 4)
+
+def shuffle_list(l : list) -> list:
+    shuffle(l)
+    return l
