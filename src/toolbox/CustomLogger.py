163b39d99267d75d5d125731d9f731859c6c98ec
AXLMRIN <axelmorinmail@gmail.com>
2025-06-14 16:07:56 +0200

    Added log method

--- src/toolbox/CustomLogger.py
+++ src/toolbox/CustomLogger.py
@@ -5,8 +5,26 @@ import smtplib
 from .secrets import EMAIL_FROM, EMAIL_TO, EMAIL_FROM_PWD, URL_ONYXIA
 # SCRIPTS ######################################################################
 class CustomLogger:
-    def __init__(self):
+    def __init__(self, text_filename : str = None):
         self.name = ""
+        self.text_filename = text_filename
+
+    def log(self, message, printing : bool = False):
+        if printing:
+            print(message)
+        # Save the message in the log
+        if self.text_filename is None:
+            print("-- No log file available --")
+        else: 
+            try : 
+                #log file already exists
+                with open(self.text_filename, "a") as file:
+                    file.write(message)
+            except : 
+                # log file does not exist
+                with open(self.text_filename, "w") as file:
+                    file.write(message)
+                    
 
     def notify_when_done(self, message : str = '') : 
         """send an email when finished"""
