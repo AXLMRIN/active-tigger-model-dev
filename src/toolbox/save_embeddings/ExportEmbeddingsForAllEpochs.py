a390313e1955abe963e62f5d3a537cf2a5d93874
AXLMRIN <axelmorinmail@gmail.com>
2025-06-14 13:51:24 +0200

    initial commit

---
+++ src/toolbox/save_embeddings/ExportEmbeddingsForAllEpochs.py
@@ -0,0 +1,24 @@
+# IMPORTS ######################################################################
+import os
+from .ExportEmbeddingsForOneEpoch import ExportEmbeddingsForOneEpoch
+# SCRIPTS ######################################################################
+class ExportEmbeddingsForAllEpochs:
+    """
+    """
+    def __init__(self, foldername) -> None:
+        """
+        """
+        self.__foldername : str = foldername
+        self.__n_epochs : int = len(
+            [f for f in os.listdir(foldername) if f.startswith("checkpoint")])
+        self.__results : list[dict] = []
+
+        def export_all(self, device : str|None = None):
+            """
+            """
+            for epoch in range(self.__n_epochs) : 
+                ExportEmbeddingsForOneEpoch(self.__foldername, epoch, device).\
+                    routine()
+        
+        def routine(self, device : str|None = None) -> None: 
+            self.export_all(device)
---
+++ src/toolbox/save_embeddings/ExportEmbeddingsForOneEpoch.py
@@ -0,0 +1,76 @@
+# IMPORTS ######################################################################
+from datasets import load_from_disk, Dataset, DatasetDict, concatenate_datasets
+from transformers import AutoModelForSequenceClassification
+from torch import Tensor, no_grad, cat, save
+from torch.cuda import is_available as cuda_available
+from ..general import checkpoint_to_load
+import os
+# SCRIPTS ######################################################################
+class ExportEmbeddingsForOneEpoch: 
+    """
+    """
+    def __init__(self, foldername: str, epoch : int, 
+        device : str|None = None) -> None:
+        """
+        """
+        self.__foldername : str = foldername
+        self.__epoch : str = epoch
+        self.__ds : DatasetDict = load_from_disk(f"{foldername}/data/")
+
+        if device is None : 
+            self.device = "cuda" if cuda_available() else "cpu"
+        else :
+            self.device = device
+
+        checkpoint : str = checkpoint_to_load(foldername, epoch)
+        self.__model = AutoModelForSequenceClassification.\
+            from_pretrained(f"{foldername}/{checkpoint}")
+        
+        if not(os.path.exists(f"{foldername}/embeddings/epoch_{epoch}")):
+            os.makedirs(f"{foldername}/embeddings/epoch_{epoch}")
+    
+    def __get_embeddings(self, dataset : Dataset) -> tuple[Tensor, Tensor]:
+        with no_grad():
+            embeddings = None
+            labels = None
+
+            for batch in dataset.batch(16): 
+                batch_labels = Tensor(batch["labels"]).int().to(device=self.device)
+                model_input = {
+                    key : Tensor(batch[key]).int().to(device=self.device)
+                    for key in ['input_ids', 'attention_mask']
+                }
+                batch_embeddings = self.__model.base_model(**model_input).\
+                    last_hidden_state[:,0,:].squeeze()
+
+                if embeddings is None:
+                    embeddings = batch_embeddings
+                else :
+                    embeddings = cat((embeddings,batch_embeddings), axis = 0)
+
+                if labels is None:
+                    labels = batch_labels
+                else :
+                    labels = cat((labels,batch_labels), axis = 0)
+        return labels, embeddings
+
+    def export_train_embeddings(self):
+        """
+        """
+        train_dataset : Dataset = concatenate_datasets(
+            (self.__ds["train"], self.__ds["eval"]))
+        labels, embeddings = self.__get_embeddings(train_dataset)
+        save(labels, f"{self.__foldername}/embeddings/epoch_{self.__epoch}/train_labels.pt")
+        save(embeddings, f"{self.__foldername}/embeddings/epoch_{self.__epoch}/train_embeddings.pt")
+
+    def export_test_embeddings(self):
+        """
+        """
+        labels, embeddings = self.__get_embeddings(self.__ds["test"])
+        save(labels, f"{self.__foldername}/embeddings/epoch_{self.__epoch}/test_labels.pt")
+        save(embeddings, f"{self.__foldername}/embeddings/epoch_{self.__epoch}/test_embeddings.pt")
+
+    def routine(self) -> None:
+        self.export_train_embeddings()
+        self.export_test_embeddings()
+    
