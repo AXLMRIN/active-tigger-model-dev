316a5e49ab91814d189bb6f6e2915cfe63238422
AXLMRIN <axelmorinmail@gmail.com>
2025-06-16 09:54:51 +0200

    nSamples

--- src/toolbox/optimize_classifiers/DataHandlerForGOfSC.py
+++ src/toolbox/optimize_classifiers/DataHandlerForGOfSC.py
@@ -8,17 +8,25 @@ from .. import ROOT_MODELS
 class DataHandlerForGOfSC:
     """
     """
-    def __init__(self, foldername : str) -> None:
+    def __init__(self, foldername : str, n_samples : int|None = None) -> None:
         """
         """
         if foldername.startswith("./") : 
             root = foldername
         else :
             root = f"{ROOT_MODELS}/{foldername}"
+
         self.X_train : np.ndarray = load(f"{root}/train_embeddings.pt",
                             weights_only=True).cpu().numpy()
-        self.y_train : np.ndarray = load(f"{root}/train_labels.pt",
+        labels : np.ndarray = load(f"{root}/train_labels.pt",
                             weights_only=True).cpu().numpy()
+        self.y_train = [np.argmax(row).item() for row in labels]
+
+        # Resample
+        if n_samples is not None : 
+            self.X_train, self.y_train = resample(self.X_train,self.y_train, 
+                                                  n_samples=n_samples)
+            
         self.X_test : np.ndarray = load(f"{root}/test_embeddings.pt",
                             weights_only=True).cpu().numpy()
         self.y_test : np.ndarray = load(f"{root}/test_labels.pt",
