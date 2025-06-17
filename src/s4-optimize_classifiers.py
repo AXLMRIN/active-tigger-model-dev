3ed654c06599505af3f95c6d1c9d3c420927eaef
AXLMRIN <axelmorinmail@gmail.com>
2025-06-17 14:08:09 +0200

    .

--- src/s4-optimize_classifiers.py
+++ src/s4-optimize_classifiers.py
@@ -1,9 +1,8 @@
 from toolbox import RoutineGOfKNN
 
 ranges_of_configs = {
-    "learning_rate" : [5e-5, 5e-4, 2e-5],
-    "weight_decay" : [0.05, 0.01], 
-    "epoch" : [1,2]
+    "learning_rate" : [1e-5,5e-5,1e-4],
+    "epoch" : [1,2,3]
 }
 
 routine = RoutineGOfKNN(
@@ -12,4 +11,4 @@ routine = RoutineGOfKNN(
     n_samples = 500
 )
 
-routine.routine("2025-06-16-TEST-RoutineKNN.csv", n_iterations = 2)
+routine.routine("2025-06-17-TEST-RoutineKNN.csv", n_iterations = 2)
