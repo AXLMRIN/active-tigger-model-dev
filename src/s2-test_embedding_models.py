f5fd7dfcec9c3c1ce339c17e4e2b3ec4c5e58eb1
AXLMRIN <axelmorinmail@gmail.com>
2025-06-17 17:11:07 +0200

    added a classifier tag

--- src/s2-test_embedding_models.py
+++ src/s2-test_embedding_models.py
@@ -1,6 +1,5 @@
 from toolbox import TestAllEpochs
 
-
 TestAllEpochs("./models/google-bert/bert-base-uncased/001").\
     routine("2025-06-14-TEST.csv", 
             additional_tags = {"classifier" : "Baseline - HF Classifier"})
