1811aa5e5f3d15275d22f667a6a021495c1678ab
AXLMRIN <axelmorinmail@gmail.com>
2025-06-18 15:12:02 +0200

    initial commit

---
+++ src/toolbox/visualise_results/Table.py
@@ -0,0 +1,149 @@
+# IMPORTS ######################################################################
+import pandas as pd
+from ..general import SUL_string, get_band, pretty_mean_and_ci
+from plotly.graph_objs._figure import Figure
+import plotly.graph_objects as go
+from great_tables import GT, style, loc
+# CONSTANT #####################################################################
+LIGHT_GREY = "#F0F0F0"
+# SCRIPTS ######################################################################
+class Table : 
+    """
+    """
+    def __init__(self, 
+        data_baseline : pd.DataFrame, 
+        data_others : pd.DataFrame, 
+        column_row : str = "classifier", 
+        column_column : str = "epoch",
+        column_color : str = "embedding_model",
+        column_score : str = "score", 
+        measure : str = "f1_macro", 
+        column_measure : str = "measure") -> None:
+        """
+        """
+        self.__data_baseline : pd.DataFrame = data_baseline
+        self.__data_others : pd.DataFrame = data_others
+        self.__column_row : str = column_row
+        self.__column_column : str = column_column
+        self.__column_color : str = column_color
+        self.__column_score : str = column_score
+        self.__measure : str = measure
+        self.__column_measure : str = column_measure
+        self.__fig = None
+
+    def preprocess(self, alpha : float = 0.9) -> None:
+        """
+        """
+        # Select columns
+        columns_to_retrieve = [col for col in [self.__column_row,
+        self.__column_column, self.__column_color, self.__column_score, 
+            self.__column_measure] if col is not None]
+        
+        self.__data_baseline = self.__data_baseline.\
+            loc[:,columns_to_retrieve]
+        self.__data_others = self.__data_others.\
+            loc[:,columns_to_retrieve]
+        # There is no need to keep the dataset appart, so we are merging them from now
+        data = pd.concat((self.__data_baseline, self.__data_others))
+
+        # Select rows where the measure matches
+        measure_condition = data[self.__column_measure] == self.__measure
+        data = data.loc[measure_condition, :]
+
+        # Compute Mean and Condidence Intervals of for the score
+        get_lower_band = lambda col : get_band(col, "lower", alpha)
+        get_upper_band = lambda col : get_band(col, "upper", alpha)
+        columns_to_groupby : list[str] = [col 
+            for col in [self.__column_color, self.__column_row, self.__column_column]
+            if col is not None
+        ]
+        data_M_and_CI = data.\
+            groupby(columns_to_groupby, as_index = False).\
+            agg(
+                mean = (self.__column_score, "mean"),
+                lower_band = (self.__column_score, get_lower_band),
+                upper_band = (self.__column_score, get_upper_band)
+            )
+        data_M_and_CI["text"] = data_M_and_CI.\
+            apply(pretty_mean_and_ci, axis = 1)
+        
+        # Drop unnecessary columns
+        data_M_and_CI = data_M_and_CI.\
+            loc[:,[self.__column_color, self.__column_column, self.__column_row,
+                "text"]]
+        
+        # Only work with strings
+        for column in data_M_and_CI : 
+            #NOTE Raises an error that makes no sense
+            data_M_and_CI.loc[:,column] = data_M_and_CI.loc[:,column].apply(str)
+        
+        self.__list_of_columns = SUL_string(data_M_and_CI[self.__column_column])
+
+        data_table_format = []
+        data_M_and_CI_grouped = data_M_and_CI.groupby([self.__column_color, self.__column_row], as_index = False)
+        for (color, row), sub_df in data_M_and_CI_grouped:
+            data_table_format.append({
+                self.__column_color : color,
+                self.__column_row : row,
+                **{
+                    column : sub_df.loc[
+                        sub_df[self.__column_column] == column,
+                        "text"
+                    ].item()
+                    for column in self.__list_of_columns
+                }
+            })
+        self.__data_table_format = pd.DataFrame(data_table_format)
+    
+    def build_table(self) -> None:
+        """
+        """
+        best_results : dict[str:list[int]] = {}
+        for col in self.__list_of_columns : 
+            best_results[col] = []
+            for row in range(len(self.__data_table_format)):
+                value_cell = self.__data_table_format.iloc[row][col]
+                value_cell = float(value_cell.split("±")[0])
+                values_row = self.__data_table_format.iloc[row][self.__list_of_columns]
+                values_row = [float(value.split("±")[0]) for value in values_row]
+                max_values_row = max(values_row)
+                if  value_cell == max_values_row:
+                    best_results[col].append(row)
+
+        self.__fig = (
+            GT(self.__data_table_format)
+            .tab_stub(
+                rowname_col=self.__column_row,
+                groupname_col=self.__column_color
+            )
+            .tab_stubhead(
+                label = self.__column_row
+            )
+            .tab_spanner(
+                label = self.__column_column, 
+                columns = self.__list_of_columns
+            )
+            .tab_style(
+                style = style.text(align = "center"),
+                locations = loc.column_header()
+            )
+            .opt_horizontal_padding(scale = 3)
+            .opt_vertical_padding(scale = 1.5)
+            .tab_style(
+                style = style.text(color="Navy", weight = "bold"),
+                locations= [
+                    loc.body(columns=[col], rows = best_results[col])
+                    for col in best_results
+                ]
+            )
+            .tab_style(
+                style = [style.fill(color=LIGHT_GREY), style.text(weight = "bold",size="large")],
+                locations= loc.row_groups()
+            )
+
+        )
+
+    def routine(self, alpha : float = 0.9) -> None:
+        self.preprocess(alpha)
+        self.build_table()
+        return self.__fig
