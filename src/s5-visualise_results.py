8bfde377b819c86d9a6b8ae74d165da9c9315228
AXLMRIN <axelmorinmail@gmail.com>
2025-06-18 10:40:29 +0200

    .

--- src/s5-visualise_results.py
+++ src/s5-visualise_results.py
@@ -1,7 +1,7 @@
 from toolbox import VisualiseAll
 
 
-VA = VisualiseAll(filename_baseline = "2025-06-16-TEST-MODEL.csv", 
-    filename_others = "2025-06-16-TEST-RoutineKNN.csv")
+VA = VisualiseAll(filename_baseline = "2025-06-14-TEST.csv", 
+    filename_others = "2025-06-17-TEST-RoutineKNN.csv")
 
 VA.routine()
