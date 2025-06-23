82e2d0a72053c1ebed0d94cf9dd8615de7699138
AXLMRIN <axelmorinmail@gmail.com>
2025-06-23 18:13:24 +0200

    change the roots

--- src/s5-visualise_results.py
+++ src/s5-visualise_results.py
@@ -1,7 +1,7 @@
 from toolbox import VisualiseAll
 
 
-VA = VisualiseAll(filename_baseline = "2025-06-14-TEST.csv", 
-    filename_others = "2025-06-17-TEST-RoutineKNN.csv")
+VA = VisualiseAll(filename_baseline = "./results/debug/2025-06-14-TEST.csv", 
+    filename_others = "./results/debug/2025-06-17-TEST-RoutineKNN.csv",)
 
-VA.routine()
+VA.routine(foldername = "./figures/debug")
