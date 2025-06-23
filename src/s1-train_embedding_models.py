a0d171efcdb2ed33927ef10e37babdbf07b57cec
AXLMRIN <axelmorinmail@gmail.com>
2025-06-23 10:31:42 +0200

    implemented the logs (runnging)

--- src/s1-train_embedding_models.py
+++ src/s1-train_embedding_models.py
@@ -1,16 +1,18 @@
 from toolbox import DataHandler, CustomTransformersPipeline, clean, CustomLogger
 
 DH, pipe = None, None
+logger = CustomLogger("./custom_logs")
 try : 
     DH = DataHandler(
         filename = "./data/316_ideological_book_corpus/ibc.csv",
         text_column = "sentence", 
         label_column = "leaning",
-        logger = CustomLogger("./custom_logs")
+        logger = logger
     )
     DH.routine(stratify_columns="LABEL")
     pipe = CustomTransformersPipeline(data = DH, 
-        model_name = "FacebookAI/roberta-base")
+        model_name = "FacebookAI/roberta-base", 
+        logger = logger)
     pipe.routine(debug_mode=True)
 
 except Exception as error: 
