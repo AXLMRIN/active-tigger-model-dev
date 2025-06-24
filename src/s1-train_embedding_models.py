48e80c6e04c8153a6fb12105f648b5afbd742301
AXLMRIN <axelmorinmail@gmail.com>
2025-06-24 12:21:49 +0000

    loop

--- src/s1-train_embedding_models.py
+++ src/s1-train_embedding_models.py
@@ -2,24 +2,35 @@ from toolbox import DataHandler, CustomTransformersPipeline, clean, CustomLogger
 
 DH, pipe = None, None
 logger = CustomLogger("./custom_logs")
-try : 
-    DH = DataHandler(
-        filename = "./data/316_ideological_book_corpus/ibc.csv",
-        text_column = "sentence", 
-        label_column = "leaning",
-        logger = logger
-    )
-    DH.routine(stratify_columns="LABEL")
-    pipe = CustomTransformersPipeline(data = DH, 
-        model_name = "FacebookAI/roberta-base", 
-        logger = logger)
-    pipe.routine(debug_mode=True)
 
-except Exception as error: 
-    print("### ERROR " + "#" * 40)
-    print(error)
-    print("#" * 50)
+LEARNING_RATES = [1e-6, 1e-5, 5e-5, 1e-4]
+MODELS = [
+    "FacebookAI/roberta-base", 
+    "google-bert/bert-base-uncased", 
+    "answerdotai/ModernBERT-base"
+]
+for learning_rate in LEARNING_RATES:
+    for model in MODELS :
+        try : 
+            DH = DataHandler(
+                filename = "./data/media_ideology_split.csv",
+                text_column = "content", 
+                label_column = "bias_text",
+                logger = logger
+            )
+            DH.routine(stratify_columns="LABEL")
+            pipe = CustomTransformersPipeline(
+                data = DH, 
+                model_name = model,
+                learning_rate = learning_rate,
+                logger = logger)
+            pipe.routine(debug_mode = False)
 
-finally:
-    del DH, pipe
-    clean()
+        except Exception as error: 
+            print("### ERROR " + "#" * 40)
+            print(error)
+            print("#" * 50)
+
+        finally:
+            del DH, pipe
+            clean()
