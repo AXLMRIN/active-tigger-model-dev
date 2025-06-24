12f979aa6e038644cb278c699ef5cb4ab3bb2186
AXLMRIN <axelmorinmail@gmail.com>
2025-06-24 12:22:24 +0000

    KNN Routine

--- src/s4-optimize_classifiers.py
+++ src/s4-optimize_classifiers.py
@@ -2,16 +2,26 @@ from toolbox import RoutineGOfKNN, CustomLogger
 
 logger = CustomLogger("./custom_logs")
 
+LEARNING_RATES = [1e-6, 1e-5, 5e-5, 1e-4]
+MODELS = [
+    "FacebookAI/roberta-base", 
+    "google-bert/bert-base-uncased", 
+    # "answerdotai/ModernBERT-base"
+]
+
 ranges_of_configs = {
-    "learning_rate" : [1e-5,5e-5,1e-4],
-    "epoch" : [1,2,3]
+    "learning_rate" : LEARNING_RATES,
+    "epoch" : [1,2,3, 4, 5]
 }
 
-routine = RoutineGOfKNN(
-    foldername = "./models/FacebookAI/roberta-base",
-    ranges_of_configs = ranges_of_configs,
-    n_samples = 500, 
-    logger = logger
-)
+for model in MODELS:
+    routine = RoutineGOfKNN(
+        foldername = f"./models/{model}",
+        ranges_of_configs = ranges_of_configs,
+        n_samples = 1000, 
+        logger = logger
+    )
 
-routine.routine("2025-06-17-TEST-RoutineKNN.csv", n_iterations = 2)
+    routine.routine("./results/333_results/2025-06-23-Others.csv", n_iterations = 3)
+
+logger.notify_when_done("Optimise KNN done")
