40f6d77db585b5c95c4af0733fad2e56dff192b6
AXLMRIN <axelmorinmail@gmail.com>
2025-06-24 12:21:59 +0000

    write a loop

--- src/s2-test_embedding_models.py
+++ src/s2-test_embedding_models.py
@@ -2,6 +2,16 @@ from toolbox import TestAllEpochs, CustomLogger
 
 logger = CustomLogger("./custom_logs")
 
-TestAllEpochs("./models/FacebookAI/roberta-base/001", logger = logger).\
-    routine("2025-06-14-TEST.csv", 
-            additional_tags = {"classifier" : "Baseline - HF Classifier"})
+MODELS = [
+    "FacebookAI/roberta-base", 
+    "google-bert/bert-base-uncased", 
+    "answerdotai/ModernBERT-base"
+]
+
+for model in MODELS :
+    for i in range(1,5):
+        TestAllEpochs(f"./models/{model}/00{i}", logger = logger).\
+            routine(
+                "./results/333_results/HuggingFace_Baseline.csv", 
+                additional_tags = {"classifier" : "Baseline - HF Classifier"}
+            )
