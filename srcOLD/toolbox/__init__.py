d40c58ecd4cea2c6d5801cca5f1ed152ebeacfbb
AXLMRIN <axelmorinmail@gmail.com>
2025-06-30 18:32:32 +0200

    deleted src old

--- srcOLD/toolbox/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dataOBJECT import DATA
-from .optimizerOBJECT import optimize_classifier
-from .CustomLogger import CustomLogger
-from .cMapperOBJECT import cMapper
-from .routinesOBJECTS import routine
-from .routinesNotOptimisedOBJECTS import routineNotOptmisied
--- srcOLD/toolbox/cMapperOBJECT.py
+++
@@ -1,8 +0,0 @@
-class cMapper:
-    def __init__(self,keys, functions):
-        self.keys = keys
-        self.functions = functions
-
-    def __call__(self, idx, value):
-        try: return {self.keys[idx] : self.functions[idx](value)}
-        except: raise(KeyError, "custom mapping idx not right")
